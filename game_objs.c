431235b523bd1f88c025be6d916c753c1cfa45ff
Ethan <ethan.hawk@valpo.edu>
2021-05-27 00:20:53 -0500

    Hopefully fixed the segfault...

--- game_objs.c
+++ game_objs.c
@@ -4,14 +4,18 @@ void remove_entity_from_list(entity_list_T *l, entity_list_node_T *entity) {
 	entity_list_node_T* curr = l->head;
 	entity_list_node_T* next = curr->next;
 	entity_list_node_T* prev = curr;
-	do {
-		prev = curr;
-		curr = next;
-		next = next->next;
-	} while (curr != entity);
-	prev->next = curr->next;
-	curr = NULL;
-	return;
+	if (curr == entity) {
+		l->head = next;
+		free(curr);
+	} else {
+		do {
+			prev = curr;
+			curr = next;
+			next = next->next;
+		} while (curr != entity);
+		prev->next = next;
+		free(curr);
+	}
 }
 
 void add_entity_to_list(entity_list_T *l, entity_list_node_T *entity_node) {
