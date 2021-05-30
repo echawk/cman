884e576540a2dfd47fd123a9038761958d14d346
Ethan <ethan.hawk@valpo.edu>
2021-05-30 16:42:24 -0500

    game_utils.c; Added Documentation describing what each function does in game_utils

--- game_utils.c
+++ game_utils.c
@@ -11,6 +11,23 @@
 #define PILL_T  2
 
 
+/*
+	Inputs:
+	list - a pointer to a linked list of entities
+	player - a pointer to a player entity
+	score - a pointer to the score of the game
+	ny - the next y value for the player
+	nx - the next x value for the player
+
+	This function is responsible for printing out the list of entities, as well as
+	performing collision detection. The reason it also does collision detection,
+	is because it saves us an iteration through each respective linked list; we
+	already have to go through every element once, so we might as well do collision
+	detection here as well.
+
+	TODO:
+	- add in color support
+*/
 void print_entity_list(entity_list_T *list, entity_T *player, int *score, int *ny, int *nx) {
 	entity_list_node_T *node  = (entity_list_node_T *) malloc(sizeof(entity_list_node_T));
 	entity_list_node_T *node2 = (entity_list_node_T *) malloc(sizeof(entity_list_node_T));
@@ -49,6 +66,17 @@ void print_entity_list(entity_list_T *list, entity_T *player, int *score, int *n
 	*/
 }
 
+/*
+	Inputs:
+	list - a pointer to an empty entity list
+	type - the type of entities to create
+	icon - the entities icon
+	max_y - the max value in the y direction for the game board
+	max_x - the max value in the x direction for the game board
+
+	This function takes an empty entity list and fills it up with entities of a specified
+	type. It makes a call to set_coordinates to determine the coordinates for each entity
+*/
 void init_entity_list(entity_list_T *list, int type, char *icon, int max_y, int max_x){
 	int i = 0;
 	int number_entities = 0;
@@ -77,6 +105,23 @@ void init_entity_list(entity_list_T *list, int type, char *icon, int max_y, int 
 	}
 }
 
+/*
+	Inputs:
+	new - a pointer to an enitiy list node
+	max_y - the max y value
+	max_x - the max x value
+	list - a pointer to an entity list
+
+	This function is responsible for 'terrain' generation. It takes in an entity and figures
+	out where to stick it on the map. For PILL_T entities, it just picks a random coordinate,
+	so the list pointer is never accessed. For WALL_T entities, it does some random seeding
+	accross the map, but with a higher weight to 'attaching' to the head of the list, wherever
+	that may be
+
+	TODO:
+	- make the generation more robust for walls; implement a list length function, then pick
+	a random inside the list to attach to instead of just the head.
+*/
 void set_coordinates(entity_list_node_T *new, int max_y, int max_x, entity_list_T *list) {
 	new->value.x = (int) rand() % max_x;
 	new->value.y = (int) rand() % max_y;
@@ -108,6 +153,20 @@ void set_coordinates(entity_list_node_T *new, int max_y, int max_x, entity_list_
 			break;
 	}
 }
+
+/*
+	Inputs:
+	player - a pointer to an enitiy
+	dy - a pointer to store the delta y for the direction
+	dx - a pointer to store the delta x for the direction
+	direction - a char to determine what direction the player should be heading
+
+	This fuction is responsible for determining both the character sprite for the
+	player based off of the direction that is given, but also updates the proper x
+	and y deltas for the player to move in the given direction.
+
+	TODO:
+*/
 void update_player_entity(entity_T *player, short *dy, short *dx, char direction) {
 	switch(direction) {
 		case 'u':
@@ -135,4 +194,3 @@ void update_player_entity(entity_T *player, short *dy, short *dx, char direction
 			break;
 	}
 }
-
